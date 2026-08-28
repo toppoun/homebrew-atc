7ffd43b8bde9836838d388468bf44b56e00a2faa
toppoun <217878558+toppoun@users.noreply.github.com>
2026-08-28 17:33:54 +0900

    Update atc to v0.1.0-rc.7

--- Formula/atc.rb
+++ Formula/atc.rb
@@ -1,9 +1,9 @@
 class Atc < Formula
   desc "Fast AtCoder workflow from your terminal"
   homepage "https://github.com/toppoun/atc-rs"
-  url "https://github.com/toppoun/atc-rs/releases/download/v0.1.0-rc.6/atc-v0.1.0-rc.6-macos-aarch64.tar.gz"
-  version "0.1.0-rc.6"
-  sha256 "ebbf3fb31f624e4f5d01875ffaafcd40323c05b7bde32d9b3b956503f107ddaf"
+  url "https://github.com/toppoun/atc-rs/releases/download/v0.1.0-rc.7/atc-v0.1.0-rc.7-macos-aarch64.tar.gz"
+  version "0.1.0-rc.7"
+  sha256 "bdb0a34ce5397da8d2b9edd2d588f33f498a6e0dd643390187ed8979ea5c539e"
   license "MIT"
 
   depends_on :macos
