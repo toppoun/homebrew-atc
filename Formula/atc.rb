d5dbc60b18f81fe419b041d467ec69efb1557aae
toppoun <217878558+toppoun@users.noreply.github.com>
2026-09-12 10:37:22 +0900

    rc.8のversion修正

--- Formula/atc.rb
+++ Formula/atc.rb
@@ -2,7 +2,7 @@ class Atc < Formula
   desc "Fast AtCoder workflow from your terminal"
   homepage "https://github.com/toppoun/atc-rs"
   url "https://github.com/toppoun/atc-rs/releases/download/v0.1.0-rc.8/atc-v0.1.0-rc.8-macos-aarch64.tar.gz"
-  version "0.1.0-rc.7"
+  version "0.1.0-rc.8"
   sha256 "991199fe30eaa92cc7164515854a29cf8ec6b6ab08408a921d072b84f2834979"
   license "MIT"
 
